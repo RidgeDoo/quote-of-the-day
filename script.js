7f1fee11ffde76784529aae50172c952f9cde177
ridge <ridgebreak@gmail.com>
2024-05-09 10:09:03 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.btn');
+let notes = document.querySelectorAll('.input-box');
+
+
+createBtn.addEventListener('click', ()=>{
+    let inputbox = document.createElement('p');
+    let img = document.createElement('img');
+    inputbox.className = 'input-box';
+    inputbox.setAttribute('contenteditable', 'true');
+    img.src = 'image/delete.png';
+    notesContainer.appendChild(inputbox).appendChild(img);
+})
